928b948c56f7b5b946fc051658c5696772c737c5
Sam <sammobeau@gmail.com>
2022-05-28 15:19:16 -0600

    added buttons

---
+++ src/components/Welcome/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <>
+      <div>
+        <button classname='btn'>
+          Log In
+        </button>
+        <button className='btn'>
+          Register
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Button
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Welcome.css';
+import Button from './Button';
 
 const Welcome = () => {
   return (
     <div className='container'>
         <welcome className ="welcome">
             <hi>Welcome</hi>
+            <Button />
         </welcome>
     </div>
   )
