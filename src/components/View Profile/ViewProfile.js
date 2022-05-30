40d47879414e143eae1a7ab4c51d5eb4c988a49e
Sam <sammobeau@gmail.com>
2022-05-30 16:56:28 -0600

    css styling

--- src/components/View Profile/ViewProfile.js
+++ src/components/View Profile/ViewProfile.js
@@ -1,27 +1,30 @@
 import React from "react";
+import './style.css'
 
 function ViewProfile(){
     return(
-      <div className="container">
-        <form action="">
-            <label for="fname">First name:</label>
-            <input type="text" id="fname" name="fname" value="John"></input>
-            <label for="lname">Last name:</label>
-            <input type="text" id="lname" name="lname" value="Doe"></input>
-            <input type="submit" value="Submit"></input>
-        </form> 
-        <form action="">
-            <label for="fname">Education</label>
-            <select name="edLevel" id="edLevel">
-                <option value="None"> </option>
-                <option value="HighSchool">HighSchool Diploma</option>
-                <option value="Certification">Certification</option>
-                <option value="Bachelors">Bachelors Degree</option>
-                <option value="Masters">Masters Degree</option>
-                <option value="PhD">PhD</option>
-            </select>
-            <input type="submit" value="Submit"></input>
-        </form> 
+      <div className="form">
+        <div className="form-body">
+          <form action="">
+              <label className="form__label" for="fname">First name:</label>
+              <input className="form__input" type="text" id="fname" name="fname" value="John"></input>
+              <label className="form__label" for="lname">Last name:</label>
+              <input className="form__input" type="text" id="lname" name="lname" value="Doe"></input>
+                <input className="form__submit" type="submit" value="Submit"></input>
+          </form> 
+          <form action="">
+              <label className="form__label" for="fname">Education:</label>
+              <select className="form__input" name="edLevel" id="edLevel">
+                  <option value="None"> </option>
+                  <option value="HighSchool">HighSchool Diploma</option>
+                  <option value="Certification">Certification</option>
+                  <option value="Bachelors">Bachelors Degree</option>
+                  <option value="Masters">Masters Degree</option>
+                  <option value="PhD">PhD</option>
+              </select>
+                <input className="form__submit" type="submit" value="Submit"></input>
+          </form>
+        </div> 
       </div>  
     );
 }
