c6c18c5a5e805ec817fae0e373de824ef7afe003
Kierra Gagnon <78625238+kierrarg@users.noreply.github.com>
2022-05-30 15:41:47 -0600

    will work idk?

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,7 +3,7 @@ import './style.css'
 import { useNavigate } from 'react-router-dom';
 function Register() {
     let navigate = useNavigate();  
-
+    
     const [firstName, setFirstName] = useState(null);
     const [lastName, setLastName] = useState(null);
     const [email, setEmail] = useState(null);
@@ -34,35 +34,40 @@ function Register() {
         console.log(firstName,lastName,email,password,confirmPassword);
     }
 
-   return(
-        <div className="form">
-            <div className="form-body">
-                <div className="username">
-                    <label className="form__label" for="firstName">First Name </label>
-                    <input className="form__input" type="text" value={firstName} onChange = {(e) => handleInputChange(e)} id="firstName" placeholder="First Name"/>
-                </div>
-                <div className="lastname">
-                    <label className="form__label" for="lastName">Last Name </label>
-                    <input  type="text" name="" id="lastName" value={lastName}  className="form__input" onChange = {(e) => handleInputChange(e)} placeholder="LastName"/>
-                </div>
-                <div className="email">
-                    <label className="form__label" for="email">Email </label>
-                    <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
-                </div>
-                <div className="password">
-                    <label className="form__label" for="password">Password </label>
-                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
-                </div>
-                <div className="confirm-password">
-                    <label className="form__label" for="confirmPassword">Confirm Password </label>
-                    <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
-                </div>
-            </div>
-            <div class="footer">
-                <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
-            </div>
-        </div>
-       
+    return(
+      <div className="form">
+          <div className="form-body">
+              <div className="username">
+                  <label className="form__label" for="firstName">First Name </label>
+                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
+              </div>
+              <div className="lastname">
+                  <label className="form__label" for="lastName">Last Name </label>
+                  <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
+              </div>
+              <div className="email">
+                  <label className="form__label" for="email">Email </label>
+                  <input  type="email" id="email" className="form__input" placeholder="Email"/>
+              </div>
+              <div className="password">
+                  <label className="form__label" for="password">Password </label>
+                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
+              </div>
+              <div className="confirm-password">
+                  <label className="form__label" for="confirmPassword">Confirm Password </label>
+                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
+              </div>
+          </div>
+          <div class="footer">
+              <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
+          </div>
+          <div class="footer">
+            <button onClick={() => {navigate("/")}} className='btn'>
+            Back
+            </button>
+          </div>
+
+      </div>      
     )       
 }
 export default Register;
